f160336225af7dc7644dacc3c57e402002122ece
Final_Zenix <mutandina169@gmail.com>
2023-11-25 17:03:03 +0100

    First Commits

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{vue,js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{vue,js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{vue,js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{vue,js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#BD9D49',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
